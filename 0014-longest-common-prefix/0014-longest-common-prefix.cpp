1b6eb545f1af06a2fb811b1ecc8b8b53c9d30c0b
Harsh kumar <23bcs11475@cuchd.in>
2026-08-07 11:16:11 +0530

    Time: 0 ms (100%), Space: 11.8 MB (57.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,75 +1,22 @@
-class trienode{
-    public:
-    char d;
-    trienode* arr[26];
-    bool t;
-    int c;
-
-    trienode( char d){
-        this ->d=d;
-        for(int i=0;i<26;i++){
-            arr[i]=NULL;
-        }
-        this->t=false;
-        this->c=0;
-    }
-};
-class trie{
-    public:
-    trienode*root;
-    trie(){                             
-        root=new trienode('\0');
-    }
-
-    void insertutil(trienode*root,string word){
-        if(word.length()==0){
-            root->t=true;
-            return ;
-        }
-        trienode* child;
-        int i=word[0]-'a';
-        if(root->arr[i]!=NULL){
-            child=root->arr[i];
-        }
-        else{
-            child= new trienode(word[0]);
-            root->arr[i]=child;
-            root->c++;
-        }
-        insertutil(child,word.substr(1));
-    }
-
-    void insert(string word){
-        insertutil(root,word);
-    }
-};
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";   
-     int count=0;
-     trie*t=new trie();
-     for(int i=0;i<strs.size();i++){
-        t->insert(strs[i]);
-     }   
-    string s=strs[0];
-    string ans="";
-    trienode*root=t->root;
-    if(root->t)
-    return "";
-     for(int i=0;i<s.size();i++){
-        char ch=s[i];
-        if(root->c==1){
-            ans.push_back(ch);
-            int j=ch -'a';
-            root=root->arr[j];
-
+        int r=strs[0].size();
+        int c=strs.size();
+        bool a=true;
+        string ans ="";
+        for(int i=0;i<r;i++){
+            char ch=strs[0][i];
+            for(int j=1;j<c;j++){
+                if(ch!=strs[j][i]){
+                    a = false;
+                    break;
+                }
+            }
+            if(a==false) break;
+            else ans.push_back(ch);
+            
         }
-        else{
-            break;
-        }
-        if(root->t==true) break;
-     }
-     return ans;
+        return ans;
     }
 };
