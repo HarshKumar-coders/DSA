a3da5b06019fa47792356bd156cda3d351c188fa
Harsh kumar <23bcs11475@cuchd.in>
2026-08-29 11:10:17 +0530

    Time: 0 ms (100%), Space: 8.4 MB (83.45%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int reverse(int x) {
-        long  int ans=0;
-        while(x!=0){
-        int digit=x%10;
-        if((ans>INT_MAX/10)||(ans<INT_MIN/10)){
-            return 0;
+        int ans = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check positive overflow
+            if (ans > INT_MAX / 10 || 
+                (ans == INT_MAX / 10 && digit > 7))
+                return 0;
+
+            // Check negative overflow
+            if (ans < INT_MIN / 10 || 
+                (ans == INT_MIN / 10 && digit < -8))
+                return 0;
+
+            ans = ans * 10 + digit;
         }
-        ans=(ans*10)+digit;
-        x=x/10;
-        }
-        
+
         return ans;
     }
 };
